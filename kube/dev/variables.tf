0460bfab6e2bccf932d6764c8103010f6e0ce4ca
Indhumathi1 <mathiindhu2594@gmail.com>
2021-11-20 12:25:15 +0530

    sending vpc code

---
+++ kube/dev/variables.tf
@@ -0,0 +1,21 @@
+variable "clustersg" {
+
+}
+variable "block1" {
+
+}
+variable "block2" {
+
+}
+variable "block3" {
+
+}
+variable "block4" {
+
+}
+variable "block5" {
+
+}
+variable "block6" {
+
+}
