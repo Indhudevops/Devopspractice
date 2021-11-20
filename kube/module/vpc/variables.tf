a1cc3a765e8dd2ca84658275689055b031b7a687
Indhumathi1 <mathiindhu2594@gmail.com>
2021-11-20 12:30:31 +0530

    sending module file

---
+++ kube/module/vpc/variables.tf
@@ -0,0 +1,22 @@
+variable "clustersg" {
+  description = "this is the name of securitygroup"
+  type        = string
+}
+variable "block1" {
+  type        = string
+}
+variable "block2" {
+  type        = string
+}
+variable "block3" {
+  type        = string
+}
+variable "block4" {
+  type        = string
+}
+variable "block5" {
+  type        = string
+}
+variable "block6" {
+  type        = string
+}
